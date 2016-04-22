e900bff033e772b2fe0e926b91238ddfff35ada0
Amanda Rincon <anrincon@ufl.edu>
2016-04-22 15:50:45 -0700

    remove title from custom full page template

--- page_fullwidth.php
+++ page_fullwidth.php
@@ -22,7 +22,6 @@ Template name: Full Width
             <?php if (have_posts()) :
                 /* OUR DATA CONTEXT IS DEFINED  */
                 while (have_posts()) : the_post(); ?>
-                    <h2><?php the_title(); ?></h2>
                     <?php the_content();
                 endwhile;
             endif; ?>
