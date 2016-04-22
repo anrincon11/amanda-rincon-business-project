ca555ab0eb974b837c38471863fe4cdc077c7764
Amanda Rincon <anrincon@ufl.edu>
2016-04-22 15:06:49 -0700

    add welcome message to home page

--- index.php
+++ index.php
@@ -1,26 +1,13 @@
 <?php get_header(); ?>
 
 		<section class="row">
-			<div class="nine columns">
-				<?php
-					if ( have_posts() ) {
-					    while ( have_posts() ) {
-					        the_post(); ?>
-							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-							<?php
-							if ( has_post_thumbnail() ) {
-                				the_post_thumbnail('thumbnail');
-            				}
-							?>
-							<?php the_excerpt();?>
-							<a href="<?php the_permalink(); ?>">Read More...</a>
-					    <?php } // end while
-					} // end if
-				?>
-			</div>
-			<div class="three columns">
-				<h4>Sidebar</h4>
-				<p>Sidebar</p>
+			<div class="twelve columns">
+
+				<h1>Welcome to Love By Designs</h1>
+
+				<p>My name is Amanda Rincon, and I am the lead graphic and web designer. I started this business after discovering what my two passions are in life. I am a sucker for love and everything it encompasses, and I absolutely enjoy designing websites. So I figured, why not combine the two!</p>
+
+				<p>With Love By Designs I hope to provide you with the opportunity to express your love in a way that you never thought possible. My mission is to help you create a beatuifully unique website through an incredible experience that will be unforgetable. Together we can build something amazing. Your engagement and wedding day are special and should be treated as such. They are memories that will last forever and should be shared for a lifetime.</p>
 			</div>
 		</section>
 
