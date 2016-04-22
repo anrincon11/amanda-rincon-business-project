2e649359f700a03b99182b51a93ab0882503384b
Amanda Rincon <anrincon@ufl.edu>
2016-04-22 15:52:15 -0700

    remove title from page file

--- page.php
+++ page.php
@@ -9,7 +9,6 @@
 		<div class="nine columns">
             <?php if (have_posts()) :
                 while (have_posts()) : the_post(); ?>
-                    <h2><?php the_title(); ?></h2>
 					<?php the_content();
                 endwhile;
             endif; ?>
