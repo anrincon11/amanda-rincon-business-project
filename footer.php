d323420ba8bbec36315e7eb77ec7ef1bfe1a4e1e
Amanda Rincon <anrincon@ufl.edu>
2016-04-22 15:42:33 -0700

    add footer copyright information

--- footer.php
+++ footer.php
@@ -1,13 +1,7 @@
 <!-- BEGIN FOOTER -->
 	<footer class="row">
-		<div class="four columns">
-			<?php dynamic_sidebar('footer-left'); ?>
-		</div>
-		<div class="four columns">
-			<?php dynamic_sidebar('footer-middle'); ?>
-		</div>
-		<div class="four columns">
-			<?php dynamic_sidebar('footer-right'); ?>
+		<div class="twelve columns">
+			<p>Copyright 2016. Love By Designs. All Rights Reserved.</p>
 		</div>
 	</footer>
 </div>
