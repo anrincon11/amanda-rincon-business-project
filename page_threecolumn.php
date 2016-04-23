2bf19b35287e28f9a82196595a983b26c2f84158
Amanda Rincon <anrincon@ufl.edu>
2016-04-23 10:17:04 -0700

    add custom three column page to project

---
+++ page_threecolumn.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template name: Three Column
+*/
+?>
+
+<?php get_header(); ?>
+<section class="row three-column-class">
+
+	<div class="nine columns">
+
+		<div class="three columns">
+			<h2>Basic</h2>
+			<h4>$25 per hour</h4>
+			<p>Template based web design</p>
+			<p>Includes up to 5 pages</p>
+			<p>1 design concepts</p>
+			<p>Contact form</p>
+		</div>
+
+		<div class="three columns">
+			<h2>Premium</h2>
+			<h4>$40 per hour</h4>
+			<p>Custom designed website</p>
+			<p>Unlimited pages</p>
+			<p>2 design concepts</p>
+			<p>Contact form</p>
+		</div>
+
+		<div class="three columns">
+			<h2>Elite</h2>
+			<h4>$60 per hour</h4>
+			<p>Custom designed website</p>
+			<p>Unlimited pages</p>
+			<p>2 design concepts</p>
+			<p>Contact form</p>
+			<p>Graphic design services</p>
+		</div>
+
+	</div>
+
+</section>
+
+<?php get_footer(); ?>
