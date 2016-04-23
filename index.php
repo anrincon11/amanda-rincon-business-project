d7488698fbdd5525a9e6a51a154f98636dc831bc
Amanda Rincon <anrincon@ufl.edu>
2016-04-23 12:09:31 -0700

    adjust code for hero image to make it work

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php get_header(); ?>
 
-		<section class="row">
+	<figure id="hero">
+		<img src="<?php echo get_template_directory_uri () . '/img/proposal.jpg'; ?>" alt="proposal" />
+	</figure>
 
-			<div class="twelve columns">
+	<section class="row">
 
-				<h1>Welcome to Love By Designs</h1>
+		<div class="twelve columns">
 
-				<p>My name is Amanda Rincon, and I am the lead graphic and web designer. I started this business after discovering what my two passions are in life. I am a sucker for love and everything it encompasses, and I absolutely enjoy designing websites. So I figured, why not combine the two!</p>
+			<h1>Welcome to Love By Designs</h1>
 
-				<p>With Love By Designs I hope to provide you with the opportunity to express your love in a way that you never thought possible. My mission is to help you create a beatuifully unique website through an incredible experience that will be unforgetable. Together we can build something amazing. Your engagement and wedding day are special and should be treated as such. They are memories that will last forever and should be shared for a lifetime.</p>
-			</div>
-		</section>
+			<p>My name is Amanda Rincon, and I am the lead graphic and web designer. I started this business after discovering what my two passions are in life. I am a sucker for love and everything it encompasses, and I absolutely enjoy designing websites. So I figured, why not combine the two!</p>
+
+			<p>With Love By Designs I hope to provide you with the opportunity to express your love in a way that you never thought possible. My mission is to help you create a beatuifully unique website through an incredible experience that will be unforgetable. Together we can build something amazing. Your engagement and wedding day are special and should be treated as such. They are memories that will last forever and should be shared for a lifetime.</p>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
