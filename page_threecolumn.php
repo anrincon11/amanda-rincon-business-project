ff21df2c7fc20ab0f7cdd8b9fcce5575527ec175
Amanda Rincon <anrincon@ufl.edu>
2016-04-23 10:41:24 -0700

    adjust grid layout for pricing page

--- page_threecolumn.php
+++ page_threecolumn.php
@@ -5,38 +5,35 @@ Template name: Three Column
 ?>
 
 <?php get_header(); ?>
-<section class="row three-column-class">
 
-	<div class="nine columns">
+<section id="pricing" class="row">
 
-		<div class="three columns">
-			<h2>Basic</h2>
-			<h4>$25 per hour</h4>
-			<p>Template based web design</p>
-			<p>Includes up to 5 pages</p>
-			<p>1 design concepts</p>
-			<p>Contact form</p>
-		</div>
+	<div class="four columns">
+		<h2>Basic</h2>
+		<h4>$25 per hour</h4>
+		<p>Template based web design</p>
+		<p>Includes up to 5 pages</p>
+		<p>1 design concepts</p>
+		<p>Contact form</p>
+	</div>
 
-		<div class="three columns">
-			<h2>Premium</h2>
-			<h4>$40 per hour</h4>
-			<p>Custom designed website</p>
-			<p>Unlimited pages</p>
-			<p>2 design concepts</p>
-			<p>Contact form</p>
-		</div>
+	<div class="four columns">
+		<h2>Premium</h2>
+		<h4>$40 per hour</h4>
+		<p>Custom designed website</p>
+		<p>Unlimited pages</p>
+		<p>2 design concepts</p>
+		<p>Contact form</p>
+	</div>
 
-		<div class="three columns">
-			<h2>Elite</h2>
-			<h4>$60 per hour</h4>
-			<p>Custom designed website</p>
-			<p>Unlimited pages</p>
-			<p>2 design concepts</p>
-			<p>Contact form</p>
-			<p>Graphic design services</p>
-		</div>
-
+	<div class="four columns">
+		<h2>Elite</h2>
+		<h4>$60 per hour</h4>
+		<p>Custom designed website</p>
+		<p>Unlimited pages</p>
+		<p>2 design concepts</p>
+		<p>Contact form</p>
+		<p>Graphic design services</p>
 	</div>
 
 </section>
