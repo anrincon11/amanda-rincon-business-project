d4d6e88970907f8e6bb997941470c8877b0ce7b0
Amanda Rincon <anrincon@ufl.edu>
2016-04-25 15:37:32 -0700

    fix spelling errors on home page

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 
 			<p>My name is Amanda Rincon, and I am the lead graphic and web designer. I started this business after discovering what my two passions are in life. I am a sucker for love and everything it encompasses, and I absolutely enjoy designing websites. So I figured, why not combine the two!</p>
 
-			<p>With Love By Designs I hope to provide you with the opportunity to express your love in a way that you never thought possible. My mission is to help you create a beatuifully unique website through an incredible experience that will be unforgetable. Together we can build something amazing. Your engagement and wedding day are special and should be treated as such. They are memories that will last forever and should be shared for a lifetime.</p>
+			<p>With Love By Designs I hope to provide you with the opportunity to express your love in a way that you never thought possible. My mission is to help you create a beautifully unique website through an incredible experience that will be unforgettable. Together we can build something amazing. Your engagement and wedding day are special and should be treated as such. They are memories that will last forever and should be shared for a lifetime.</p>
 		</div>
 
 <?php get_footer(); ?>
